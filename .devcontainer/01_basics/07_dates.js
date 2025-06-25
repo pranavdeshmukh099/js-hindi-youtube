6bf39a360bc81445f31607d821d1e1c10a1e97bc
Pranav Deshmukh <165696312+pranavdeshmukh099@users.noreply.github.com>
2025-06-25 09:10:17 +0000

    dates

---
+++ .devcontainer/01_basics/07_dates.js
@@ -0,0 +1,8 @@
+let myDate=new Date()
+console.log(myDate.toString());
+console.log(myDate.toISOString());
+console.log(myDate.toLocaleDateString());
+
+
+
+console.log(Math.floor(Date.now()/1000));
