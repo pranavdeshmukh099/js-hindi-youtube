a4ef05ddb484b8713d35f491d6bd05edf525d90f
Pranav Deshmukh <165696312+pranavdeshmukh099@users.noreply.github.com>
2025-06-25 05:39:06 +0000

    comparison

---
+++ .devcontainer/01_basics/04_comparison.js
@@ -0,0 +1,17 @@
+console.log("2">1);
+console.log(2>=1);
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined);
+
+// ===
+
+console.log("2"===2);
+
+const myFunction=function(){
+    console.log("Hello World");
+}
+console.log(myFunction);
