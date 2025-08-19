53c1984e0d2c076a3fb2d4d0e0f0a3d5749e6b2a
Pranav Deshmukh <165696312+pranavdeshmukh099@users.noreply.github.com>
2025-08-19 04:53:25 +0000

    functions

--- .devcontainer/02_basics/04_objects.js
+++ .devcontainer/02_basics/04_objects.js
@@ -5,6 +5,9 @@ const obj3={5:"a",6:"b"}
 // const obj4=Object.assign({},obj1,obj2,obj3)
 // console.log(obj4)
 
+// const obj5={...obj1,...obj2,...obj3}
+// console.log(obj5)
+
 const course ={
     corusename:"js in hindi",
     price:"999",
@@ -13,4 +16,24 @@ const course ={
 
 const {coruseInstructor:instructor}=course
 console.log(instructor)
+
+
+
+
+
+
+
+
+
+
+// const regularUser={
+//     email:"pranavdeshmukh099@gmail.com",
+//     fullName:{
+//         userFullName:{
+//             firstName:"Pranav",
+//             lastName:"Deshmukh"
+//         }
+//     }
+// }
+// console.log(regularUser.fullName.userFullName.firstName)
 
--- .devcontainer/03_basics/01_functions.js
+++ .devcontainer/03_basics/01_functions.js
@@ -1,61 +1,133 @@
-function syMyName(){
-    console.log("p");
-    console.log("p");
-    console.log("p");
-    console.log("p");
-    console.log("p");
-    console.log("p");
+// function syMyName(){
+//     console.log("p");
+//     console.log("p");
+//     console.log("p");
+//     console.log("p");
+//     console.log("p");
+//     console.log("p");
+// }
+
+// syMyName
+
+// function addTwoNumbers(number1, number2){
+//     let result=number1+number2
+//     return result
+// }
+
+// const result=addTwoNumbers(3,5)
+// //console.log("reuslt",result);
+
+
+// function loginuserMessage(username="sam"){
+//     if(username===undefined){
+//         console.log("Please enter a username");
+//         return
+//     }
+//     return `${username} just logged in`
+// }
+
+// //console.log(loginuserMessage())
+
+
+// function calculateCarPrice(...num1){
+
+//     return num1
+// }
+
+// //console.log(calculateCarPrice(200,300,400))
+
+// const user={
+//     username:"Pranav",
+//     price:99
+// }
+
+// function handleObject(anyobject){
+//     console.log(`Username is ${anyobject.username} and object price is ${anyobject.price}`);
+// }
+
+// //handleObject(user)
+
+// handleObject({
+//     username:"Sam",
+//     price:8965
+// })
+
+// const myNewArray=[200,400,100,600]
+
+// function returnSecondValue(getArray){
+//     return getArray[1]
+// }
+
+// console.log(returnSecondValue(myNewArray))
+
+
+
+
+
+function myName(){
+    console.log("P");
+    console.log("R");
+    console.log("A");
+    console.log("N");
+    console.log("A");
+    console.log("V");
 }
 
-syMyName
+// myName()
 
-function addTwoNumbers(number1, number2){
-    let result=number1+number2
-    return result
+// function addTwoNumbers(num1,num2){
+//     console.log(num1+num2);
+// }
+
+function addTwoNumbers(num1,num2){
+    // let result=num1+num2
+    // return result;
+
+    return num1+num2;
 }
 
-const result=addTwoNumbers(3,5)
-//console.log("reuslt",result);
+const result = addTwoNumbers(3,5);
+console.log("Result :", result)
 
 
-function loginuserMessage(username="sam"){
-    if(username===undefined){
-        console.log("Please enter a username");
+
+function loginUserMessage(username="sam"){
+    if(username===undefined)
+    {
+        console.log("Please enter the username");
         return
     }
     return `${username} just logged in`
 }
 
-//console.log(loginuserMessage())
+// console.log(loginUserMessage("Pranav"))
 
-
-function calculateCarPrice(...num1){
-
-    return num1
+function calculateCartPrice(...num1){
+    return num1;
 }
 
-//console.log(calculateCarPrice(200,300,400))
+// console.log(calculateCartPrice(200,400,500)
+
 
 const user={
     username:"Pranav",
-    price:99
+    price:199
 }
+
 
 function handleObject(anyobject){
-    console.log(`Username is ${anyobject.username} and object price is ${anyobject.price}`);
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
 }
 
-//handleObject(user)
-
 handleObject({
-    username:"Sam",
-    price:8965
+    username:"SAM",
+    price:399
 })
 
-const myNewArray=[200,400,100,600]
 
+const myNewArray=[200,400,100,600];
 function returnSecondValue(getArray){
-    return getArray[1]
+    return getArray[1];
 }
 
 console.log(returnSecondValue(myNewArray))
